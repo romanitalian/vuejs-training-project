9c553475429f27cc3edada3676833f5f5dd9ed83
romanitalian <romanitalian.net@gmail.com>
2019-01-22 11:36:19 +0300

    use global and local components

--- 06/js/main.js
+++ 06/js/main.js
@@ -14,6 +14,22 @@ Vue.component("app-car", {
 
 new Vue({
     el: "#app",
+    components: {
+        'app-car-local': {
+            data: function () {
+                return {
+                    cars: [
+                        {model: "BMW", speed: 132},
+                        {model: "AUDI", speed: 454},
+                        {model: "LADA", speed: 154},
+                        {model: "HONDA", speed: 313},
+                        {model: "FORD", speed: 12}
+                    ]
+                }
+            },
+            template: '<div><div class="car" v-for="car in cars"><p>{{car.model}}</p></div></div>'
+        }
+    }
 });
 
 new Vue({
