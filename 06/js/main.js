21a8bf59a214775778d3762334067ec43c0e17fb
romanitalian <romanitalian.net@gmail.com>
2019-01-22 11:24:50 +0300

    add Vue.component

--- 06/js/main.js
+++ 06/js/main.js
@@ -1,11 +1,18 @@
+Vue.component("app-car", {
+    data: function () {
+        return {
+            cars: [
+                {model: "BMW", speed: 132},
+                {model: "Audi", speed: 454},
+                {model: "Lada", speed: 154},
+                {model: "Honda", speed: 313}
+            ]
+        }
+    },
+    template: '<div><div class="car" v-for="car in cars"><p>{{car.model}}</p></div></div>'
+});
+
 new Vue({
     el: "#app",
-    data: {
-        cars: [
-            {model: "BMW", speed: 132},
-            {model: "Audi", speed: 454},
-            {model: "Lada", speed: 154},
-            {model: "Honda", speed: 313}
-        ]
-    }
+    data: {}
 });
