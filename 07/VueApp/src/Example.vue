43664ecde3bc635a40c24203c581505ccbb81c54
romanitalian <romanitalian.net@gmail.com>
2019-01-22 13:38:27 +0300

    add Example component

---
+++ 07/VueApp/src/Example.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Text example</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Example"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- 07/VueApp/src/main.js
+++ 07/VueApp/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Example from './Example.vue'
+
+Vue.component("app-example", Example);
 
 new Vue({
   el: '#app',
